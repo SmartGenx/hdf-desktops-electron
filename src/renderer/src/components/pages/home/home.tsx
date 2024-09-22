e9d22de217c0998d3e70de284dbad75c5c2f64da
H-7117 <hamod2131.a@gmail.com>
2024-09-22 15:27:55 +0300

    complete home statisc

--- src/renderer/src/components/pages/home/home.tsx
+++ src/renderer/src/components/pages/home/home.tsx
@@ -2,13 +2,22 @@ import StatistCard from '@renderer/components/statistCard'
 import StatistChartCard from './statistChartCard'
 import StatistchartTowCard from './statistchartTowCard'
 import Statistsidebar from './statistsidebar'
-import { getApi } from '@renderer/lib/http'
+import { axiosInstance, getApi } from '@renderer/lib/http'
 import { useQuery } from '@tanstack/react-query'
 import { useAuthHeader } from 'react-auth-kit'
 import { statisticCardType } from '@renderer/types'
+import { useEffect, useState } from 'react'
+
+export type statistCardInfo = {
+  diseaseCount: number
+  GovernorateCount: number
+  directoratecount: number
+  squareCount: number
+}
 
 const Home = () => {
   const authToken = useAuthHeader()
+  const [statist, setStatist] = useState<statistCardInfo | undefined>()
   const { isLoading, error, data } = useQuery({
     queryKey: ['statistics'],
     queryFn: () =>
@@ -18,7 +27,25 @@ const Home = () => {
         }
       })
   })
-  console.log('dsdsdfs', data?.data.GovernorateCount)
+
+  useEffect(() => {
+    const fetchStatist = async () => {
+      try {
+        const response = await axiosInstance.get('/statistics/Initialization', {
+          headers: {
+            Authorization: `${authToken()}`
+          }
+        })
+        setStatist(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchStatist()
+  }, [])
+
+  console.log('statist', statist)
+
   if (isLoading) return 'Loading...'
   if (error) return 'An error has occurred: ' + error.message
   return (
@@ -26,20 +53,35 @@ const Home = () => {
       <div className="grid grid-cols-4 gap-2">
         <div className=" col-span-1 ">
           <StatistCard
-            title={'المرضى'}
-            value={data?.data.GovernorateCount ?? 45}
+            title={'المرض'}
+            value={statist?.diseaseCount ?? 0}
             subtitle={'مستفيد'}
             icon={'hospital'}
           />
         </div>
         <div className="col-span-1 ">
-          <StatistCard title={'المرضى'} value={20} subtitle={'مستفيد'} icon={'house'} />
+          <StatistCard
+            title={'مديرية'}
+            value={statist?.directoratecount ?? 0}
+            subtitle={'مستفيد'}
+            icon={'house'}
+          />
         </div>
         <div className="col-span-1 ">
-          <StatistCard title={'المرضى'} value={30} subtitle={'مستفيد'} icon={'house'} />
+          <StatistCard
+            title={'المربع'}
+            value={statist?.squareCount ?? 0}
+            subtitle={'مستفيد'}
+            icon={'house'}
+          />
         </div>
         <div className="col-span-1 ">
-          <StatistCard title={'المرضى'} value={40} subtitle={'مستفيد'} icon={'house'} />
+          <StatistCard
+            title={'محافظة'}
+            value={statist?.GovernorateCount ?? 0}
+            subtitle={'مستفيد'}
+            icon={'house'}
+          />
         </div>
       </div>
       <div className="flex gap-2 mt-4 w-full ">
