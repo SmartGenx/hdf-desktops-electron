76dc50e29bc262cb0c90015db446aba7c4b78aed
H-7117 <hamod2131.a@gmail.com>
2024-10-20 10:27:37 +0300

    fixed the last edit

--- src/renderer/src/components/fileUploader.tsx
+++ src/renderer/src/components/fileUploader.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { Upload, X, Check, AlertCircle } from "lucide-react";
-import { UseFormSetValue } from "react-hook-form";
-import { Button } from "./ui/button";
+import React, { useState } from 'react'
+import { Upload, X, Check, AlertCircle } from 'lucide-react'
+import { UseFormSetValue } from 'react-hook-form'
+import { Button } from './ui/button'
 interface FileUploaderProps {
-  inputId: string;
-  isMultiple?: boolean;
-  onChange?: (files: HTMLInputElement["files"]) => void;
-  setValue: UseFormSetValue<any>;
-  fieldName?: string;
-  title?: string;
+  inputId: string
+  isMultiple?: boolean
+  onChange?: (files: HTMLInputElement['files']) => void
+  setValue: UseFormSetValue<any>
+  fieldName?: string
+  title?: string
 }
 export default function FileUploader({
   inputId,
@@ -16,18 +16,18 @@ export default function FileUploader({
   fieldName = inputId,
   title,
   onChange,
-  setValue,
+  setValue
 }: FileUploaderProps) {
-  const [uploadedFiles, setUploadedFiles] = useState<string[]>([]);
-  const [isError, setIsError] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState<string[]>([])
+  const [isError, setIsError] = useState(false)
   // const fileInputRef = useRef<HTMLInputElement>(null);
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let files = null;
+    let files = null
     if (event.target.files)
       if (event.target.files[0] != null) {
         // @ts-ignore
-        files = event?.target?.files;
-        setIsError(true);
+        files = event?.target?.files
+        setIsError(true)
       }
     if (files) {
       if (isMultiple) {
@@ -35,39 +35,36 @@ export default function FileUploader({
           setUploadedFiles((prevUploadedFiles) => [
             ...prevUploadedFiles,
             // @ts-ignore
-            file.name,
-          ]);
-        });
+            file.name
+          ])
+        })
       } else {
         // if (files && files[0] && files[0].name)
         // @ts-ignore
-        setUploadedFiles([files[0].name]);
+        setUploadedFiles([files[0].name])
       }
-      setIsError(false);
+      setIsError(false)
     }
-    onChange?.(files);
-  };
+    onChange?.(files)
+  }
   const handleFileRemove = (fileName: string) => {
-    setUploadedFiles((prevUploadedFiles) =>
-      prevUploadedFiles.filter((file) => file !== fileName)
-    );
+    setUploadedFiles((prevUploadedFiles) => prevUploadedFiles.filter((file) => file !== fileName))
     if (uploadedFiles.length === 1) {
-      setUploadedFiles([]);
+      setUploadedFiles([])
     }
-    setValue(fieldName, undefined);
-  };
+    setValue(fieldName, undefined)
+  }
   return (
     <div className="flex w-full justify-start gap-10  rounded border-2 border-dashed px-5 py-2">
       <span>{title}</span>
       <input
         multiple={isMultiple}
-
         type="file"
         name={inputId}
         id={inputId}
         className="hidden"
         onChange={(e) => {
-          handleFileChange(e);
+          handleFileChange(e)
         }}
       />
       {uploadedFiles.length != 0 ? (
@@ -78,7 +75,7 @@ export default function FileUploader({
         >
           <span className="rounded bg-gray-200 p-2 text-blue-500">رفع ملف</span>
           <Upload size={24} />
-          اسحب الملف للرفع
+          
         </label>
       ) : (
         <label
@@ -89,38 +86,34 @@ export default function FileUploader({
           <Upload size={24} />
           <span className="text-blue-500 underline decoration-2 hover:text-blue-600">
             اختر الملف
-          </span>{" "}
+          </span>{' '}
           أو اسحب الملف للرفع
         </label>
       )}
       {uploadedFiles && (
-        <div>
+        <div >
           {uploadedFiles.map((file, i) => {
             return (
               <div
                 key={i}
                 className="flex max-w-xl items-center justify-between rounded border-b px-5 py-3 text-sm text-gray-700"
               >
-                <div className="flex items-center gap-2">
+                <div className="flex items-center ">
                   {isError ? (
                     <AlertCircle className="text-red-500" size={20} />
                   ) : (
                     <Check className="text-blue-500" size={20} />
                   )}
-                  <span className="font-medium">{file}</span>
+                  <span className="font-medium text-sm ">{file}</span>
                 </div>
-                <Button
-                  size={"icon"}
-                  variant={"ghost"}
-                  onClick={() => handleFileRemove(file)}
-                >
+                <Button size={'icon'} variant={'ghost'} onClick={() => handleFileRemove(file)}>
                   <X size={20} className="text-gray-500 hover:text-gray-700" />
                 </Button>
               </div>
-            );
+            )
           })}
         </div>
       )}
     </div>
-  );
+  )
 }
