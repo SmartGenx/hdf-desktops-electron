9db4ae22ede57ea57f92eb8dda679ee0814b409e
Samithabet <sami.th71516@gmail.com>
2024-10-23 11:15:22 +0300

    refactoring:complete

--- server/database.js
+++ server/database.js
@@ -34,8 +34,8 @@ class DatabaseService {
     // Initialize both Prisma clients for local and cloud databases
     this.localPrisma = new PrismaClient({
       datasources: {
-        // db: { url: 'postgresql://postgres:sami2020@localhost:5432/Hdf_electron?schema=public' }
-        db: { url: 'postgresql://postgres:123456789@3.108.217.185:5432/hdf-web?schema=public' }
+        db: { url: 'postgresql://postgres:sami2020@localhost:5432/Hdf_electron?schema=public' }
+        // db: { url: 'postgresql://postgres:123456789@3.108.217.185:5432/hdf-web?schema=public' }
       }
     })
     this.cloudPrisma = new PrismaClient({
